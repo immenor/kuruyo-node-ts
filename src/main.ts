d478becc277db1efd56829e00510f25d52edf716
Canyon Boak <immenor@gmail.com>
2018-02-25 09:29:13 +0900

    add express; basic test get and post endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,44 @@
 import rp = require('request-promise')
 import cheerioModule = require('cheerio')
-import apn = require('apn');
+import apn = require('apn')
+import express = require('express')
+import bodyParser = require('body-parser')
 
 import { TokyuBusHTMLRepository } from "./components/TokyuBusHTMLRepository"
 import { DefaultBusLocationFactory } from "./components/DefaultBusLocationFactory"
 import { DefaultBusFactory } from "./components/DefaultBusFactory"
 
+// Setup Express
+
+const app = express()
+const router = express.Router()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// API Endpoints
+
 let htmlRepo = new TokyuBusHTMLRepository()
 
-htmlRepo.getHTML().then(function (html) {
-  let stop = new DefaultBusFactory().findStopByScanningDownFromStop(3, "守屋図書館", html)
-  console.log("Target Bus Stop", stop)
+router.get('/left-bus-locations', function(req, res) {
 
-  let locations = new DefaultBusLocationFactory().getLeftBusLocations(html)
-  console.log("All Left Bus Locations:", locations)
+  htmlRepo.getHTML().then(function (html) {
+    let locations = new DefaultBusLocationFactory().getLeftBusLocations(html)
+    res.json({ busLocations: locations })
+  })
+
 })
+
+router.post('/target-bus-stop', function (req: express.Request, res: express.Response) {
+
+  let targetStop = req.body["stop"]
+  htmlRepo.getHTML().then(function (html) {
+    let stop = new DefaultBusFactory().findStopByScanningDownFromStop(3, targetStop, html)
+    res.json({ stop: stop })
+  })
+
+})
+
+// Start Server
+
+app.use('/api', router)
+app.listen(3000, () => console.log('Kuruyo Server listening on port 3000!'))
